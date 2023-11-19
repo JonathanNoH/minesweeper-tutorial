82730489a52d8ba530fda0f426494a97c39eb07e
Jonathan Holst <97708227+JonathanNoH@users.noreply.github.com>
2023-11-19 13:23:23 -0700

    set up

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use bevy::prelude::*;
+
+#[cfg(feature="debug")]
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
+
+fn main() {
+    let mut app = App::new();
+    // Set up window
+    app
+        .add_plugins(DefaultPlugins);
+    #[cfg(feature="debug")]
+    app.add_plugins(WorldInspectorPlugin::new());
+    app.add_systems(Startup, camera_setup);
+    app.run();
+}
+
+fn camera_setup(mut commands: Commands) {
+    // 2d
+    commands.spawn(Camera2dBundle::new_with_far(0.0));
+}
